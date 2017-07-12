306590571e24b7c8df90c7c2b67370f00c8f11f0
Lucas Lima Pinheiro Silva <lucas.lima.pinheiro@hotmail.com>
2017-07-12 13:52:42 -0300

    Remake 2

---
+++ Projeto .NET Core/StudyProject/StuudyProject/src/StuudyProject/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    var ele = $("username");
+    ele.text("Fulano de tal");
+
+    var main = document.getElementById("main");
+    main.onmouseenter = function () {
+        main.style.backgroundColor = "#888";
+
+    };
+    main.onmouseleave = function () {
+        main.style.backgroundColor = "";
+
+
+    };
+})();
