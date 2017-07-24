bb5666bdf502b2a42c63dbbc57c6c3820d8d8ab5
Lucas Lima Pinheiro Silva <lucas.lima.pinheiro@hotmail.com>
2017-07-23 22:08:48 -0300

    Final
    Deployment Ajustments

--- Projeto .NET Core/StudyProject/StuudyProject/src/StuudyProject/wwwroot/js/tripEditorController.js
+++ Projeto .NET Core/StudyProject/StuudyProject/src/StuudyProject/wwwroot/js/tripEditorController.js
@@ -9,39 +9,60 @@
         vm.stops = [];
         vm.errorMessage = "";
         vm.isBusy = true;
+        vm.newStop = {};
 
-        $http.get("/api/trips/" + vm.tripName + "/stops")
+        var url = "/api/trips/" + vm.tripName + "/stops";
+
+        $http.get(url)
             .then(function (response) {
                 angular.copy(response.data, vm.stops);
+                _showMap(vm.stops);
             }, function(err){
                 vm.errorMessage = "Failed To load Stops";
             })
             .finally(function () {
             vm.isBusy = false;
-        });
-    }
-
-    function _showMap(stops) {
-
-        if (stops && stops.length > 0) {
-
-            var mapStops = _.map(stops, function (item){
-                return {
-                    lat: 50.850000,
-                    long: 4.350000,
-                    info: "Brussels, Belgium - Jun 25-27, 2014"
-                };
             });
 
-                travelMap.createMap({
-                    stops: mapStops,
-                    selector: "#map",
-                    currentStop: 1,
-                    initialZoom: 3
+        vm.addStop = function () {
+
+            vm.isBusy = true;
+
+            $http.post(url, vm.newStop)
+                .then(function (response) {
+
+                    vm.stops.push(response.data);
+                    _showMap(vm.stops);
+                    vm.newStop = {};
+                }, function (err) {
+                    vm.errorMessage = "Failed To add new Stop";
+                })
+                .finally(function () {
+                    vm.isBusy = false;
                 });
-        } else {
-            alert("vai tomar no cu o erro ta aqui");
+        };
+    }
+   
+
+    function _showMap(stops) {
+        if (stops && stops.length > 0) {
+
+            var mapStops = _.map(stops, function(item){
+                return {
+                    lat: item.latitude,
+                    long: item.longitude,
+                    info: item.name
+                };
+            });
+       
+            travelMap.createMap({
+                stops: mapStops,
+                selector: "#map",
+                currentStop: 1,
+                initialZoom: 3
+            })
         }
+
     }
 
 })();
---
+++ Projeto .NET Core/StudyProject/StuudyProject/src/StuudyProject/wwwroot/lib/_app/style.js
@@ -0,0 +1,1 @@
+!function(){$("#username").text("Fulano de tal da silva");var a=$("#sidebar,#wrapper"),e=$("#sidebarToggle i.fa");$("#sidebarToggle").click(function(){a.toggleClass("hide-sidebar"),a.hasClass("hide-sidebar")?(e.removeClass("fa-angle-left"),e.addClass("fa-angle-right")):(e.addClass("fa-angle-left"),e.removeClass("fa-angle-right"))})}();
